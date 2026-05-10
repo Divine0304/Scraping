76a8bb2b6aacd8689fd4ae9798bcb05843331ab9
chelseafankam-créatrice <chelseafankam@gmail.com>
2026-05-11 00:56:30 +0200

    Ajout configuration Selenium Guerlain

--- scraper_guerlain.py
+++ scraper_guerlain.py
@@ -6,3 +6,14 @@ from selenium.webdriver.chrome.options import Options
 BASE_URL = "https://www.amazon.fr/s?k=guerlain&page={}"
 
 NB_PAGES = 3
+options = Options()
+
+options.add_argument("--start-maximized")
+
+options.add_argument("--disable-blink-features=AutomationControlled")
+
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36"
+)
+
+driver = webdriver.Chrome(options=options)
