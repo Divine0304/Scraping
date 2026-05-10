7cb0d73a07a6a11c0559c559ca944270f2b38b99
chelseafankam-créatrice <chelseafankam@gmail.com>
2026-05-11 00:57:55 +0200

    Ajout boucle pagination Guerlain

--- scraper_guerlain.py
+++ scraper_guerlain.py
@@ -27,3 +27,14 @@ with open("guerlain_amazon.csv", "w", newline="", encoding="utf-8") as fichier:
         "categorie",
         "note"
     ])
+    for page in range(1, NB_PAGES + 1):
+
+        url = BASE_URL.format(page)
+
+        print(f"Scraping page {page}...")
+
+        driver.get(url)
+
+        time.sleep(5)
+
+        print(driver.title)
