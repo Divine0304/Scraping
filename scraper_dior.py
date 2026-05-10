95e720368add5f653533fd6dfba8f0d073e2ee30
celeste tene <mbogoumcelesta@gmail.com>
2026-05-11 00:30:49 +0200

    Ajout de la configuration de base pour le scraping sur Amazon

--- scraper_dior.py
+++ scraper_dior.py
@@ -3,3 +3,13 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+# -----------------------------
+# CONFIGURATION
+# -----------------------------
+
+BASE_URL = "https://www.amazon.fr/s?k=dior+beaute&page={}"
+NB_PAGES = 3
+
+# -----------------------------
+# SELENIUM
+# -----------------------------
