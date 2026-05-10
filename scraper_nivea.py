7f32875ccaee4f1f477b1597950215713bc316a1
chelseafankam-créatrice <chelseafankam@gmail.com>
2026-05-11 00:32:05 +0200

    Ajout création fichier CSV

--- scraper_nivea.py
+++ scraper_nivea.py
@@ -5,3 +5,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 BASE_URL = "https://www.amazon.fr/s?k=nivea&page={}"
 NB_PAGES = 3
+options = Options()
+
+options.add_argument("--start-maximized")
+options.add_argument("--disable-blink-features=AutomationControlled")
+
+driver = webdriver.Chrome(options=options)
+with open("nivea_amazon.csv", "w", newline="", encoding="utf-8") as fichier:
+
+    writer = csv.writer(fichier)
+
+    writer.writerow([
+        "nom_produit",
+        "prix",
+        "categorie",
+        "note"
+    ])
